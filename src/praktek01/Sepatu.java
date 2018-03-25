236b285bc8f4b8a87f2e8a4561cfaadd08e0b446
IkaFebrina <32430894+IkaFebrina@users.noreply.github.com>
2018-03-25 11:14:26 +0700

    Membuat Atribut dan Behavior

--- src/praktek01/Sepatu.java
+++ src/praktek01/Sepatu.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author User
  */
 public class Sepatu {
+    String Merk;
+    String Bahan_Sepatu;
+    String Ukuran;
+    String Jenis_Sepatu;
+    String Warna;
     
+    void cetakInfo(){
+        System.out.println("Merk            :"+Merk);
+        System.out.println("Bahan_Sepatu    :"+Bahan_Sepatu);
+        System.out.println("Ukuran          :"+Ukuran);
+        System.out.println("Jenis_Sepatu    :"+Jenis_Sepatu); 
+        System.out.println("Warna           :"+Warna);
+    }
 }
